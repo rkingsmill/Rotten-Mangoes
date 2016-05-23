33f1bf72ab391cb9832cbcef2bd02c2135ac0fa3
RKing <rosalyn.kingsmill@gmail.com>
2016-05-23 11:39:33 -0400

    Initial Commit

---
+++ Rotten Mangoes/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Rotten Mangoes
+//
+//  Created by Rosalyn Kingsmill on 2016-05-23.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Rotten Mangoes/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Rotten Mangoes
+//
+//  Created by Rosalyn Kingsmill on 2016-05-23.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
